434f00d81f3e398fe436681206ab3f6c4c3ba872
salsabil_55 <salsabil.tarek@dafholding.com>
2021-05-19 18:28:50 +0200

    card

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Good } from '../interfaces/good.interface';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  goods: Good[] = [
+    {
+      name : 'Smart Watch' , price: 20 , photoUrl: 'assets/img/watch.png'
+    },
+    {
+      name : 'Nike Shoes' , price: 100 , photoUrl: 'assets/img/nike.png'
+
+    },
+    {
+      name : 'Smart Headphones' , price: 50 , photoUrl: 'assets/img/headphones.png'
+
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  addToCart(index){
+    console.log('added:' ,this.goods[index]);
   }
 
 }
